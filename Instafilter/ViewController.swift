afa880c84fe67714c02b598bcb230e19b465de85
Mark Lysenko <mark.fm257@gmail.com>
2020-09-12 14:18:55 +0300

    Initial Commit

---
+++ Instafilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instafilter
+//
+//  Created by Mark (WorkProfile) on 12.09.2020.
+//  Copyright © 2020 Mark (WorkProfile). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
